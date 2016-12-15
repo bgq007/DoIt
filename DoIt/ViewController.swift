095e208388c6b300464ae87dfefab081284a5f78
BP <Billy@Williams-MacBook-Pro.local>
2016-12-14 16:59:28 -0800

    Created Dummy Tasks
    Dummy Tasks to initial populate screen

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -12,10 +12,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
+        tasks = makeTasks()
+        
         tableView.delegate = self
         tableView.dataSource = self
     
@@ -27,16 +31,28 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.name
         return cell
     
     }
     
+    func makeTasks() -> [Task] {
+            let task1 = Task()
+        task1.name = "Walk the Dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Feed the Cat"
+        task2.important = true
+        
+        return [task1, task2]
+    }
     
     
 }
