1c9c1bd51a7717dba82a44f17294cd2b30b5a909
BP <Billy@Williams-MacBook-Pro.local>
2016-12-14 16:49:20 -0800

    Table Views
    Created TableView and placed dummy text in it.

--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        tableView.delegate = self
+        tableView.dataSource = self
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
+    
+    }
+    
+    
+    
 }
 
